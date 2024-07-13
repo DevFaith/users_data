8576546e42c3f9252ae576db9d778b8efb9ef65c
DevFaith <waitherafaith@outlook.com>
2024-07-13 10:41:53 +0300

    * feat(Display.jsx): add Display component to render props jina, jina2, and jina3

---
+++ src/component/Display.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Display(props) {
+  
+  return (
+    <div>
+      <li>{props.jina}</li>
+      <li>{props.jina2}</li>
+      <li>{props.jina3}</li>
+    </div>
+  )
+}
+
+export default Display
