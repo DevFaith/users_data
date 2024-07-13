3fb0b7f5933a071103882e65c206988e7a13c621
DevFaith <waitherafaith@outlook.com>
2024-07-13 10:40:52 +0300

    * feat(App.jsx): add App component with Write and Data components

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Write from './component/Write'
+import Data from './component/Data'
+
+function App() {
+  return (
+    <>
+      <Write />
+      <Data />
+    </>
+  )
+}
+
+export default App
